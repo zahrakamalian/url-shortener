599e20ff4df6b1963b194c9b5cdc8a5441b55a3d
zahra kamalian <kamaliianzahra@gmail.com>
2026-07-01 18:34:48 +0330

    Add initial project structure with database models, migration scripts, and configuration

---
+++ src/db/models/url.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.orm import relationship
+from src.db.session import Base
+
+
+class URL(Base):
+    __tablename__ = 'urls'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, nullable=False, index=True)
+    campaign_id = Column(Integer, nullable=False, index=True)
+    original_url  = Column(String(2048), nullable=False)
+    short_code = Column(String(8), nullable=False, unique=True, index=True)
+    created_at = Column(DateTime(timezone=True),server_default=func.now(),nullable=False)
+
+    logs = relationship("Log",back_populates="url",cascade="all, delete-orphan")
+
---
+++ src/db/session.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from config import settings
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
